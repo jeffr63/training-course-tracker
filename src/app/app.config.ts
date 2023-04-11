84be17407feda40d3754a622e6282307b221a3fd
jeffr63 <jeffr63@gmail.com>
2023-04-11 07:32:12 -0500

    Use new ApplicationConfig boostrap method

---
+++ src/app/app.config.ts
@@ -0,0 +1,36 @@
+import { ApplicationConfig } from '@angular/platform-browser';
+import { environment } from '@env/environment';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { TitleStrategy, provideRouter } from '@angular/router';
+
+import { metaReducers, reducers } from './store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { APP_ROUTES } from './app.routes';
+import { AppEffects } from './store/app.effects';
+import { CustomTitleStrategyService } from './shared/services/custom-title-strategy.service';
+import { CourseEffects } from './store/course/course.effects';
+import { PathsEffects } from './store/paths/paths.effects';
+import { SourcesEffects } from './store/sources/sources.effects';
+import { UsersEffects } from './store/users/users.effects';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      StoreModule.forRoot(reducers, { metaReducers }),
+      EffectsModule.forRoot([AppEffects, CourseEffects, PathsEffects, SourcesEffects, UsersEffects]),
+      StoreDevtoolsModule.instrument({
+        maxAge: 5,
+        logOnly: environment.production,
+      })
+    ),
+    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
+    provideAnimations(),
+    provideHttpClient(),
+    provideRouter(APP_ROUTES),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, TitleStrategy } from '@angular/router';
-
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreModule } from '@ngrx/store';
-
-import { metaReducers, reducers } from '@store/index';
+import { enableProdMode } from '@angular/core';
 import { environment } from '@env/environment';
-import { APP_ROUTES } from '@app/app.routes';
+
 import { AppComponent } from '@app/app.component';
-import { AppEffects } from '@store/app.effects';
-import { CourseEffects } from '@store/course/course.effects';
-import { CustomTitleStrategyService } from '@shared/services/custom-title-strategy.service';
-import { PathsEffects } from '@store/paths/paths.effects';
-import { SourcesEffects } from '@store/sources/sources.effects';
-import { UsersEffects } from '@store/users/users.effects';
+import { appConfig } from '@app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      StoreModule.forRoot(reducers, { metaReducers }),
-      EffectsModule.forRoot([AppEffects, CourseEffects, PathsEffects, SourcesEffects, UsersEffects]),
-      StoreDevtoolsModule.instrument({
-        maxAge: 5,
-        logOnly: environment.production,
-      }),
-      ),
-    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
-    provideAnimations(),
-    provideHttpClient(),
-    provideRouter(APP_ROUTES)
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
