6b139856f2a66859f33b124275ffbafcd58a77db
Jeff Richardson <jeffr63@gmail.com>
2025-03-11 08:20:54 -0500

    Created path edit card presentational component

--- src/app/admin/path/path-edit.component.ts
+++ src/app/admin/path/path-edit.component.ts
@@ -1,48 +1,21 @@
 import { Component, DestroyRef, OnInit, inject, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Store, select } from '@ngrx/store';
 
 import * as fromRoot from '@store/index';
 import { pathsActions } from '@store/path/paths.actions';
 import { pathsFeature } from '@store/path/paths.state';
 import { Path } from '@models/paths';
+import { PathEditCardComponent } from './path-edit-card.component';
 
 @Component({
   selector: 'app-path-edit',
-  imports: [NgbModule, ReactiveFormsModule, RouterLink],
-  template: `
-    <section class="container">
-      <section class="card">
-        @if (pathEditForm) {
-        <form [formGroup]="pathEditForm">
-          <fieldset class="m-2 row">
-            <label class="col-form-label col-sm-2" for="name">Path Name</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" formControlName="name" placeholder="Enter path name" />
-              @if (pathEditForm.controls.name.errors?.required && pathEditForm.controls.name.touched) {
-              <small class="text-danger">Name is required</small>
-              }
-            </div>
-          </fieldset>
-
-          <div class="d-grid gap-2 m-2 d-sm-flex justify-content-sm-end">
-            <button class="btn btn-primary me-sm-2" (click)="save()" title="Save" [disabled]="!pathEditForm.valid">
-              <i class="bi bi-save"></i> Save
-            </button>
-            <a class="btn btn-secondary" [routerLink]="['/admin/paths']" title="Cancel">
-              <i class="bi bi-x-circle"></i> Cancel
-            </a>
-          </div>
-        </form>
-        }
-      </section>
-    </section>
-  `,
+  imports: [PathEditCardComponent],
+  template: `<app-path-edit-card [(pathEditForm)]="pathEditForm" (cancel)="cancel()" (save)="save()" />`,
   styles: [
     `
       section .card {
@@ -61,6 +34,7 @@ export default class PathEditComponent implements OnInit {
   readonly #location = inject(Location);
   readonly #store = inject(Store<fromRoot.State>);
   readonly #ref = inject(DestroyRef);
+  readonly #router = inject(Router);
 
   protected readonly id = input.required<string>();
   protected pathEditForm!: FormGroup;
@@ -83,7 +57,10 @@ export default class PathEditComponent implements OnInit {
       });
   }
 
-  save() {
+  protected cancel() {
+    this.#router.navigate(['/admin/paths']);
+  }
+  protected save() {
     this.#path.name = this.pathEditForm.controls.name.value;
     this.#store.dispatch(pathsActions.savePath({ path: this.#path }));
     this.#location.back();
