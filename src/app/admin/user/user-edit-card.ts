b54c36b1d1e4050bd413264317e94910cb2adb48
Jeff Richardson <jeffr63@gmail.com>
2025-12-12 13:29:46 -0600

    Migrated Admin User form to signal forms

--- src/app/admin/user/user-edit-card.ts
+++ src/app/admin/user/user-edit-card.ts
@@ -1,66 +1,74 @@
-import { Component, model, output } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, input, output } from '@angular/core';
+import { Field, FieldTree } from '@angular/forms/signals';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { User } from '@models/user-interface';
+import { ValidationErrors } from '@shared/components/validation-errors';
+
 @Component({
   selector: 'app-user-edit-card',
-  imports: [NgbModule, ReactiveFormsModule],
+  imports: [NgbModule, Field, ValidationErrors],
   template: `
     <section class="container">
       <section class="card">
-        @if (userEditForm()) {
-        <form [formGroup]="userEditForm()">
-          <fieldset class="m-2 row">
-            <label class="col-form-label col-sm-2" for="name">Name</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" formControlName="name" placeholder="Enter user's name" />
-              @if (userEditForm().controls.name.errors?.required) {
-              <small class="text-danger">Name is required</small>
-              }
-            </div>
-          </fieldset>
+        @if (form()) {
+          <form>
+            <fieldset class="m-2 row">
+              <label class="col-form-label col-sm-2" for="name">Name</label>
+              <div class="col-sm-6">
+                <input type="text" class="form-control" [field]="form().name" placeholder="Enter user's name" />
+                @let fname = form().name();
+                @if (fname.invalid() && fname.touched()) {
+                  <app-validation-errors [errors]="fname.errors()" />
+                }
+              </div>
+            </fieldset>
 
-          <fieldset class="m-2 row">
-            <label class="col-form-label col-sm-2" for="email">Email</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" formControlName="email" placeholder="Enter email address" />
-              @if (userEditForm().controls.email.errors?.required) {
-              <small class="text-danger">Email is required</small>
-              } @if (userEditForm().controls.email.errors?.email) {
-              <small class="text-danger">Must be a valid email</small>
-              }
-            </div>
-          </fieldset>
+            <fieldset class="m-2 row">
+              <label class="col-form-label col-sm-2" for="email">Email</label>
+              <div class="col-sm-6">
+                <input type="text" class="form-control" [field]="form().email" placeholder="Enter email address" />
+                @let femail = form().email();
+                @if (femail.invalid() && femail.touched()) {
+                  <app-validation-errors [errors]="femail.errors()" />
+                }
+              </div>
+            </fieldset>
 
-          <fieldset class="m-2 row">
-            <label class="col-form-label col-sm-2" for="email">Roles</label>
-            <div class="form-check col-sm-3" style="margin-left:20px">
-              <input type="radio" class="form-check-input" id="role1" value="admin" formControlName="role" />
-              <label class="form-check-label" for="check1">Admin</label>
-              @if (userEditForm().controls.role.errors?.required) {
-              <small class="text-danger">Role is required</small>
-              }
-            </div>
-            <div class="form-check col-sm-3">
-              <input type="radio" class="form-check-input" value="user" id="role2" formControlName="role" />
-              <label class="form-check-label" for="check1">User</label>
-            </div>
-          </fieldset>
+            <fieldset class="m-2 row">
+              <label class="col-form-label col-sm-2" for="email">Roles</label>
+              <div class="form-check col-sm-3" style="margin-left:20px">
+                <input type="radio" class="form-check-input" id="role1" value="admin" [field]="form().role" />
+                <label class="form-check-label" for="check1">Admin</label>
+              </div>
+              <div class="form-check col-sm-3">
+                <input type="radio" class="form-check-input" value="user" id="role2" [field]="form().role" />
+                <label class="form-check-label" for="check1">User</label>
+              </div>
+            </fieldset>
+            @let frole = form().role();
+            @if (frole.invalid() && frole.touched()) {
+              <div class="m-2 mt-0 row">
+                <div class="col-sm-8 offset-sm-2">
+                  <app-validation-errors [errors]="frole.errors()" />
+                </div>
+              </div>
+            }
 
-          <div class="d-grid gap-2 m-2 d-sm-flex justify-content-sm-end">
-            <button
-              class="btn btn-primary me-sm-2"
-              (click)="save.emit()"
-              title="Save"
-              [disabled]="!userEditForm().valid">
-              <i class="bi bi-save"></i> Save
-            </button>
-            <a class="btn btn-secondary" (click)="cancel.emit()" title="Cancel">
-              <i class="bi bi-x-circle"></i> Cancel
-            </a>
-          </div>
-        </form>
+            <div class="d-grid gap-2 m-2 d-sm-flex justify-content-sm-end">
+              <button
+                class="btn btn-primary me-sm-2"
+                (click)="save.emit()"
+                title="Save"
+                [disabled]="form()().invalid()">
+                <i class="bi bi-save"></i> Save
+              </button>
+              <a class="btn btn-secondary" (click)="cancel.emit()" title="Cancel">
+                <i class="bi bi-x-circle"></i> Cancel
+              </a>
+            </div>
+          </form>
         }
       </section>
     </section>
@@ -73,10 +81,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     }
     .form-buttons {
       margin-left: 3px;
-    }`,
+    }
+  `,
 })
 export class UserEditCard {
-  userEditForm = model.required<FormGroup>();
+  readonly form = input.required<FieldTree<User>>();
   cancel = output();
   save = output();
 }
--- src/app/admin/user/user-edit.ts
+++ src/app/admin/user/user-edit.ts
@@ -1,51 +1,38 @@
-import { Component, DestroyRef, OnInit, inject, input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Location } from '@angular/common';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { form } from '@angular/forms/signals';
+import { rxResource } from '@angular/core/rxjs-interop';
 
-import { Store, select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 import * as fromRoot from '@store/index';
 import { usersActions } from '@store/user/users.actions';
 import { usersFeature } from '@store/user/users.state';
-import { User } from '@models/user-interface';
+import { User, USER_EDIT_SCHEMA } from '@models/user-interface';
 import { UserEditCard } from './user-edit-card';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
   imports: [UserEditCard],
-  template: `<app-user-edit-card [(userEditForm)]="userEditForm" (cancel)="cancel()" (save)="save()" />`,
+  template: `<app-user-edit-card [form]="form" (cancel)="cancel()" (save)="save()" />`,
 })
-export default class UserEdit implements OnInit {
-  readonly #fb = inject(FormBuilder);
-  readonly #location = inject(Location);
+export default class UserEdit {
   readonly #store = inject(Store<fromRoot.State>);
-  readonly #ref = inject(DestroyRef);
   readonly #router = inject(Router);
 
   protected readonly id = input.required<string>();
-  protected userEditForm!: FormGroup;
-  #user = <User>{};
 
-  ngOnInit() {
-    this.userEditForm = this.#fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      role: ['', Validators.required],
-    });
+  readonly #user = rxResource<User, string>({
+    params: () => this.id(),
+    stream: ({ params: id }) => {
+      if (id === 'new') return of({ name: '', email: '', role: '', password: '' });
+      this.#store.dispatch(usersActions.getUser({ id: +id }));
+      return this.#store.select(usersFeature.selectCurrentUser);
+    },
+  });
 
-    this.#store.dispatch(usersActions.getUser({ id: +this.id() }));
-    this.#store
-      .pipe(select(usersFeature.selectCurrentUser))
-      .pipe(takeUntilDestroyed(this.#ref))
-      .subscribe((user: User) => {
-        this.#user = { ...user };
-        this.userEditForm.get('name').setValue(this.#user.name);
-        this.userEditForm.get('email').setValue(this.#user.email);
-        this.userEditForm.get('role').setValue(this.#user.role);
-      });
-  }
+  readonly form = form(this.#user.value, USER_EDIT_SCHEMA);
 
   cancel() {
     this.#router.navigate(['/admin/users']);
@@ -53,12 +40,12 @@ export default class UserEdit implements OnInit {
 
   save() {
     const patchData = {
-      email: this.userEditForm.controls.email.value,
-      name: this.userEditForm.controls.name.value,
-      role: this.userEditForm.controls.role.value,
+      email: this.form().value().email,
+      name: this.form().value().name,
+      role: this.form().value().role,
     };
 
-    this.#store.dispatch(usersActions.patchUser({ id: this.#user.id, user: patchData }));
-    this.#location.back();
+    this.#store.dispatch(usersActions.patchUser({ id: +this.id(), user: patchData }));
+    this.#router.navigate(['/admin/users']);
   }
 }
